97c5fcbf2cfdb4107724224b02bb82ef5b503b5a
WallFlowerBubble <bantikumar1080@gmail.com>
2022-12-04 15:12:58 +0500

    Add project files.

---
+++ IPT_PROJECT/Controllers/PostController.cs
@@ -0,0 +1,19 @@
+﻿using IPT_PROJECT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IPT_PROJECT.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Post()
+        {
+            var Post = new Post() { PostId = 101 };
+            return View(Post);
+        }
+    }
+}
---
+++ IPT_PROJECT/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IPT_PROJECT.Models
+{
+    public class Post
+    {
+        public int PostId { set; get; }
+        public String Description { set; get; }
+        public String Subject { set; get; }
+        public String CreatedBy { set; get; }
+        public DateTime dateTime { set; get; }
+
+
+    }
+}
